a5ff88a4de535e03b187d02797359869c6c47743
Nathan Standafer <standafer_na@yahoo.com>
2020-07-05 09:05:24 -0400

    it trains, but no useful results yet

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
+import numpy as np
+
 
 
 class EncoderCNN(nn.Module):
@@ -24,28 +26,22 @@ class EncoderCNN(nn.Module):
     
 
 class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, sentence_length, num_layers=1):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
         super(DecoderRNN, self).__init__()
 
         print('init DecoderRNN. embed_size: {}, hidden_size: {}, vocab_size: {}, num_layers: {}'.format(embed_size, hidden_size, vocab_size, num_layers))
 
         self.num_layers = num_layers
         self.hidden_size = hidden_size
-        self.sentence_length = sentence_length
         self.vocab_size = vocab_size
 
         # embedding layer that turns words into a vector of a specified size
         self.word_embeddings = nn.Embedding(vocab_size, embed_size)
         
-        self.lstm = nn.LSTM(embed_size, hidden_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers)
         
         # the linear layer that maps the hidden state output dimension 
         self.hidden2tag = nn.Linear(hidden_size, vocab_size)
-        
-        # initialize the hidden state (see code below)
-        #self.hidden = self.init_hidden()
-        
-        #pass
 
     def init_hidden(self, sentence_length):
         ''' Initializes hidden state '''
@@ -58,20 +54,34 @@ class DecoderRNN(nn.Module):
 
         return hidden
 
-    def forward(self, features, captions, hidden):
-        print("DecoderRNN - features.shape: {}".format(features.shape))
-        print("DecoderRNN - captions.shape: {}".format(captions.shape))
-        
+    def forward(self, features, captions):
+        #print("DecoderRNN XXXXX - features.shape: {}".format(features.shape))
+        #print("DecoderRNN - captions.shape: {}".format(captions.shape))
+        #print("DecoderRNN - captions: {}".format(captions))
+
         # ??? should I concatenate the features and captions before feeding into the LSTM?????
 
         x = self.word_embeddings(captions)
-        print("x.shape: {}".format(x.shape))
+        #print("x.shape after passing through word embeddings: {}".format(x.shape))  #torch.Size([10, 13, 256])
+
         
         # get the output and hidden state by passing the lstm over our word embeddings
         # the lstm takes in our embeddings and hiddent state
         sentence_length = captions.shape[1]
         batch_size = captions.shape[0]
         #lstm_out, hidden = self.lstm(x.view(sentence_length, 1, -1), hidden)
+
+        #now, I meed to inject the image encodings (features) into the ltsm along with the output from the embeddings.
+        #  features.shape: torch.Size([10, 256])
+        #  x.shape:        torch.Size([10, 13, 256])
+        features_reshaped = torch.reshape(features, (features.shape[0], 1, features.shape[1]))
+        #print("DecoderRNN - features_reshaped.shape: {}".format(features_reshaped.shape))
+        x = torch.cat((features_reshaped, x), dim=1)
+        #print("DecoderRNN - x.shape after catenation: {}".format(x.shape))
+
+
+        hidden = self.init_hidden(sentence_length+1)
+
         lstm_out, hidden = self.lstm(x, hidden)
         
         #outputs should be a PyTorch tensor with size [batch_size, captions.shape[1], vocab_size]
@@ -79,16 +89,19 @@ class DecoderRNN(nn.Module):
         
         # get the scores for the most likely tag for a word
         #??? how to feed outut of RNN into linear layber(s).  Do I just 
-        print("lstm_out.shape: {}".format(lstm_out.shape))
+        #print("lstm_out.shape: {}".format(lstm_out.shape))
         # shape output to be (batch_size*seq_length, hidden_dim)
         lstm_out_contiguous = lstm_out.contiguous().view(-1, self.hidden_size)
-        print("lstm_out_contiguous.shape: {}".format(lstm_out_contiguous.shape))
+        #print("lstm_out_contiguous.shape: {}".format(lstm_out_contiguous.shape))
 
         fc_outputs = self.hidden2tag(lstm_out_contiguous)
-        print("fc_outputs.shape: {}".format(fc_outputs.shape))
+        #print("fc_outputs.shape: {}".format(fc_outputs.shape))
 
         outputs = fc_outputs.view(batch_size, -1, self.vocab_size) 
-        print("outputs.shape: {}".format(outputs.shape))
+        #print("outputs.shape: {}".format(outputs.shape))
+
+        #remove first output word:
+        outputs = outputs[:,1:,:]
 
         outputs_scores = F.log_softmax(outputs, dim=2)
         return outputs_scores
@@ -96,4 +109,31 @@ class DecoderRNN(nn.Module):
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        print("sample inputs.shape: {}".format(inputs.shape))
+        print("sample inputs[0]: {}".format(inputs[0]))
+
+        captions_np = np.zeros([inputs.shape[0], 1])
+        captions = torch.from_numpy(captions_np).type(torch.LongTensor).to("cuda")
+
+        output = self.forward(inputs[0], captions)
+
+        output_np = output.cpu().detach().numpy()
+        probs = np.exp(output_np)
+        print("probs:  {}".format(probs))
+        output_list = [0]
+
+        for i in range(max_len):
+            next_word = np.argmax( probs[0,-1][1:] ) #largest index that isn't index 0
+            #print("next_word: {}".format(next_word))
+
+            captions_np = np.append(captions_np, [[next_word]], axis=1)
+            captions = torch.from_numpy(captions_np).type(torch.LongTensor).to("cuda")
+
+            #print("current captions: {}".format(captions))
+            output = self.forward(inputs[0], captions)
+            output_np = output.cpu().detach().numpy()
+            probs = np.exp(output_np)
+
+            output_list.append(int(next_word))
+
+        return output_list
